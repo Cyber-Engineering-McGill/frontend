00cd06bb1de365b1f0ff318d686e7d77a1c86519
yxL05 <yang.xiang.64@hotmail.com>
2026-06-08 22:20:13 -0400

    Deleted backend section.

--- src/app/shop/contexts/initcontexts.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { createContext } from "react";
-import { ProductDescType, ProductQtyContextType } from "../types/producttype";
-import { ProductType } from "../types/producttype";
-
-export const ItemsContext = createContext<ProductType[] | undefined>(undefined);
-export const ItemsQtysContext = createContext<ProductQtyContextType | undefined>(undefined);
-export const ItemsDescsContext = createContext<ProductDescType[] | undefined>(undefined);
